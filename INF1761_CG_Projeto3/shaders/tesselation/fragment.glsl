ad04c105f83f1ab4c869f6bca8a5929a8d72a78d
PDTCCLF <80176084+PDTCCLF@users.noreply.github.com>
2023-12-03 08:17:02 -0300

    Atualização nos shaders de tesselation

--- INF1761_CG_Projeto3/shaders/tesselation/fragment.glsl
+++ INF1761_CG_Projeto3/shaders/tesselation/fragment.glsl
@@ -1,48 +1,30 @@
 #version 410
 
-uniform vec4 lamb;
-uniform vec4 ldif;
-uniform vec4 lspe;
-uniform samplerCube sky;
-uniform vec4 mamb;
-uniform vec4 mdif;
-uniform vec4 mspe;
-uniform float mshi;
-uniform float mopacity;
-uniform float fdensity;
-uniform vec4 fcolor;
-uniform sampler2D decal;
-uniform vec4 cpos;
-in data {
-  vec3 normal;
-  vec3 view;
+const float ka=0.2f;
+const vec4 ma=vec4(0.2f,0.2f,0.2f,1.0f);
+const vec4 md=vec4(1.0f,1.0f,1.0f,1.0f);
+const vec4 ms=vec4(1.0f,1.0f,1.0f,1.0f);
+const float shi=64.0f;
+in data{
+  vec3 neye;
+  vec3 veye;
   vec3 light;
   vec2 texcoord;
-  vec4 pos;
 } f;
+
+uniform sampler2D earth;
 
 out vec4 color;
 
-void main (void) 
+void main(void)
 {
-  vec3 normal = normalize(f.normal);
-  vec3 view = normalize(-f.pos.xyz);
-  vec3 light = normalize(f.light);
-  float ndotl = dot(normal, light);
-  
-  color = mamb * lamb + mdif * ldif * max(0, ndotl) * texture(decal, f.texcoord); 
-
-  if (ndotl > 0) {
-    vec3 refl = normalize(reflect(-light, normal));
-    color += mspe * lspe * pow(max(0, dot(refl, view)), mshi); 
+  vec3 vnorm=normalize(-f.veye);
+  vec3 nnorm=normalize(f.neye);
+  vec3 lnorm=normalize(f.light);
+  float ndotl=dot(nnorm,lnorm);
+  color=(ma*ka+md*max(0,ndotl))*texture(earth,f.texcoord);
+  if (ndotl>0){
+    vec3refl=normalize(reflect(-lnorm,nnorm));
+    color+=ms*pow(max(0,dot(refl,vnorm)),shi);
   }
-
-  color.a = mopacity;
-
-  // Fog calculation
-    color = texture(sky, f.pos.xyz);
-    float fog = exp(-pow(fdensity * distance(f.pos,cpos), 2));
-    color = fog * color + (1.0 - fog) * fcolor;
-
-  
 }
--- INF1761_CG_Projeto3/shaders/tesselation/vertex.glsl
+++ INF1761_CG_Projeto3/shaders/tesselation/vertex.glsl
@@ -1,35 +1,8 @@
 #version 410
 
-layout(location = 0) in vec4 pos;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec2 texcoord;
-
-uniform vec4 lpos;  // light pos in lighting space
-uniform vec4 cpos;  // camera pos in lighting space
-uniform mat4 Mv; 
-uniform mat4 Mn; 
-uniform mat4 Mvp;
-
-out data {
-  vec3 normal;
-  vec3 view;
-  vec3 light;
-  vec2 texcoord;
-  vec4 pos;
-
-} v;
-
-void main (void) 
+layout(location=0) in vec4 geom;
+out vec4 pgeom;
+void main(void)
 {
-  vec3 p = vec3(Mv * pos);
-  if (lpos.w == 0) 
-    v.light = normalize(vec3(lpos));
-  else 
-    v.light = normalize(vec3(lpos) - p); 
-  v.view = normalize(vec3(cpos) - p);
-  v.normal = normalize(vec3(Mn * vec4(normal, 0.0f)));
-  v.texcoord = texcoord;
-  v.pos= pos;
-
-  gl_Position = Mvp * pos; 
+    pgeom=geom;
 }
